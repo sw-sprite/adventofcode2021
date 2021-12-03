638d1ba4ce59327a30266e6975383bba41c1591f
sw-sprite <wei980622@gmail.com>
2021-12-03 13:47:01 -0600

    solution for d3p2, had to rewrite cause didnt realize the lcb and mcb are dynamic as item removed...

--- d3/power_2.py
+++ d3/power_2.py
@@ -8,25 +8,35 @@ with open("input.txt") as f:
 
 b_length = len(lines[0][:-1])
 
-count_arr = [0] * b_length
-input_size = len(lines)
-print(count_arr)
+# oxy find most, if eql use 1, co2 is opposite, most/least changes as array change
+# eg: if pos 5 most common could be 1 in first loop, then 0 second loop
+oxy_td_arr = lines
+co2_td_arr = lines
 
-for x in lines:
-    current = x[:-1]
-    for y in range(b_length):
-        # counting 0s
-        if x[y] == "0":
-            count_arr[y] += 1
-
-for x in range(b_length):
-    if count_arr[x] > (input_size - count_arr[x]):
-        gamme_b += "0"
-        epsilon_b += "1"
+# findning most/least common in the array at given index
+# need this bc array size change during run time
+def fmc(i, arr):
+    zero = one = 0
+    for x in arr:
+        if int(x[i]) == 0: zero += 1 
+        else: one += 1
+    return 1 if one >= zero else 0
+def flc(i, arr):
+    zero = one = 0
+    for x in arr:
+        if int(x[i]) == 0: zero += 1 
+        else: one += 1
+    return 0 if one >= zero else 1
+def remove_mismatch(reference, index, data_list):
+    if len(data_list) > 1:
+        new_data_list = [i for i in data_list if int(i[index]) == reference]
+        return new_data_list
     else:
-        gamme_b += "1"
-        epsilon_b += "0"
+        return data_list
 
+# have to determine when the sample size cut down to 1, build 2d array with each step
+for x in range(b_length):
+    oxy_td_arr = remove_mismatch(fmc(x, oxy_td_arr), x, oxy_td_arr)
+    co2_td_arr = remove_mismatch(flc(x, co2_td_arr), x, co2_td_arr)
 
-ans = int(gamme_b, 2) * int(epsilon_b, 2)
-print(ans)
+print(int(co2_td_arr[0], 2) * int(oxy_td_arr[0], 2))
