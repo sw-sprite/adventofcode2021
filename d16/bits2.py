12f4e5868e891701556c6824840e44cc0bcf4e26
sw-sprite <wei980622@gmail.com>
2021-12-16 01:50:13 -0600

    took way too long because I deleted 2 leading 0 from input and spent 2 hours debugging...

---
+++ d16/bits2.py
@@ -0,0 +1,95 @@
+# import binascii
+import numpy as np
+with open("input.txt") as f:
+    lines = f.read()
+
+lines = lines.strip("\n")
+# integer = int(lines, 16)
+# spec = '{fill}{align}{width}{type}'.format(fill='0', align='>', width=len(lines)*4, type='b')
+# bi_string = format(integer, spec)
+
+p1_sum = 0
+
+def to_binary(ipt):
+    integer = int(ipt, 16)
+    spec = '{fill}{align}{width}{type}'.format(fill='0', align='>', width=len(ipt)*4, type='b')
+    bi_string = format(integer, spec)
+    return bi_string
+
+# parse packets, add up version numbers
+def calc(bs):
+    global p1_sum
+    version, type_id, content = bs[0:3], bs[3:6], bs[6:]
+    p1_sum += int(version,2)
+    print("═══════════════════════════════════════════════════")
+    # print("entering calc with: ", bs)
+    # print(int(version, 2), type_id, content)
+
+    # a literal
+    if type_id == "100": 
+        bi_val = ""
+        i = 0
+        while True:
+            cur_window = content[0+i:5+i]
+            bi_val += cur_window[1:5]
+            if cur_window[0] == '0':
+                break
+            i += 5
+            # print(cur_window)
+            print(bi_val, i)
+
+        print("exiting with literal pkg value:", int(bi_val,2))
+        print(bs[i+11:])
+        # 11 is 6(header), plus 5 which is the window we just read
+        return int(bi_val, 2), bs[i+11:] 
+
+    # operator
+    else: 
+        length_type_id = content[0]
+        subpackets = [] 
+        if length_type_id == "0":
+            length = int(content[1:16],2)
+            op_content = content[16:16+length]
+            leftover = content[16+length:]
+            while op_content:
+                # print("cur subpkg length: ", len(op_content), op_content)
+                subpackets.append(None)
+                subpackets[-1], op_content = calc(op_content)
+        if length_type_id == "1":
+            num_subpkg = int(content[1:12],2)
+            op_content = content[12:] # 1 + 11
+            # print(length)
+            for _ in range(num_subpkg):
+                subpackets.append(None)
+                subpackets[-1], op_content = calc(op_content)
+            leftover = op_content
+        
+        if type_id == "000":
+            subpackets = sum(subpackets)
+        elif type_id == "001":
+            subpackets = np.prod(subpackets)
+        elif type_id == "010":
+            subpackets = min(subpackets)
+        elif type_id == "011":
+            subpackets = max(subpackets)
+        elif type_id == "101":
+            subpackets = 1 if subpackets[0] > subpackets[1] else 0
+        elif type_id == "110":
+            subpackets = 1 if subpackets[0] < subpackets[1] else 0
+        elif type_id == "111":
+            subpackets = 1 if subpackets[0] == subpackets[1] else 0
+        return subpackets, leftover
+
+
+
+# calc(to_binary("D2FE28"))
+# calc(to_binary("38006F45291200"))
+# calc(to_binary("EE00D40C823060"))
+# calc(to_binary("8A004A801A8002F478"))
+# calc(to_binary("620080001611562C8802118E34"))
+# calc(to_binary("C0015000016115A2E0802F182340"))
+# calc(to_binary("A0016C880162017C3686B18A3D4780"))
+
+pt2 = calc(to_binary(lines))
+# print("pt 1 version sum: ", p1_sum)
+print(pt2[0])
